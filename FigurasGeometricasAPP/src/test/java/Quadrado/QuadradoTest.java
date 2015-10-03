a39a7996061ad78d7a7ad4fd18c3433cb75eec23
Fabio Cabral <fabiomoreiracabral@gmail.com>
2015-10-03 11:17:09 -0700

    test class Quadrado

---
+++ FigurasGeometricasAPP/src/test/java/Quadrado/QuadradoTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Quadrado;
+
+import br.edu.fasam.figurasgeometricasapp.Quadrado;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author herlan
+ */
+public class QuadradoTest extends TestCase {
+    
+    public QuadradoTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    // TODO add test methods here. The name must begin with 'test'. For example:
+    // public void testHello() {}
+    
+        public void testeCalcularPerimetro()
+        {
+            Quadrado quadrado = new Quadrado(5);
+            assertEquals(20.19, quadrado.getPerimetro());
+        }
+        
+        public void testCalcularArea()
+        {
+            Quadrado quadrado = new Quadrado(5);
+            assertEquals(25.30, quadrado.getArea());
+            
+        }
+}
