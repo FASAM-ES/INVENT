5f35403a99995f0bb073e037ed7045ee1e4907b4
Carlos Augusto <carlos.augusto@inventsoftware.com.br>
2015-10-03 11:14:21 -0300

    Bora trabalhar galeragit add .git add .git add .

---
+++ FigurasGeometricasAPP/src/main/java/br/edu/fasam/figurasgeometricasapp/FiguraMain.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fasam.figurasgeometricasapp;
+
+/**
+ *
+ * @author carlos
+ */
+
+public class FiguraMain {
+public static void main(String[] args){
+System.out.println("Bora Trabalhaaaaarrrrrrr");
+}
+}
