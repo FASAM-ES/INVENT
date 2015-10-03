a3f430cffae254f9a5fe636081c795104884bdd0
Carlos Augusto <carlosaugustovazrodrigues@gmail.com>
2015-10-03 11:36:12 -0300

    implementacao da classe quadrado (refs#1111 @1h)

---
+++ FigurasGeometricasAPP/src/main/java/br/edu/fasam/figurasgeometricasapp/Retangulo.java
@@ -0,0 +1,31 @@
+package br.edu.fasam.figurasgeometricasapp;
+
+/**
+ *
+ * @author carlos
+ */
+public class Retangulo {
+    Integer base;
+    Integer altura;
+
+    public Integer getBase() {
+        return base;
+    }
+
+    public void setBase(Integer base) {
+        this.base = base;
+    }
+
+    public Integer getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Integer altura) {
+        this.altura = altura;
+    }
+    
+    public int CalcularArea(int base, int altura)
+    {
+        return base*altura;
+    }
+}
